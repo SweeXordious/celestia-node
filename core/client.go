ef49e69d0578f2ac7d5a52cefa729d2b954f4bb4
sweexordious <chamirachid1@gmail.com>
2024-11-09 02:01:21 +0400

    temp

--- core/client.go
+++ core/client.go
@@ -1,11 +1,7 @@
 package core
 
 import (
-	"crypto/tls"
 	"fmt"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
-
 	coregrpc "github.com/tendermint/tendermint/rpc/grpc"
 )
 
@@ -14,7 +10,5 @@ type Client = coregrpc.BlockAPIClient
 
 // NewRemote creates a new Client that communicates with a remote Core endpoint over gRPC.
 func NewRemote(ip, port string) (Client, error) {
-	return coregrpc.StartBlockAPIGRPCClient(fmt.Sprintf("tcp://%s:%s", ip, port), grpc.WithTransportCredentials(credentials.NewTLS(&tls.Config{
-		MinVersion: tls.VersionTLS12,
-	})))
+	return coregrpc.StartBlockAPIGRPCClient(fmt.Sprintf("tcp://%s:%s", ip, port))
 }
